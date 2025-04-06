d6f329dff36e5d4458a23ca161ee7dd7f6e91da8
Sofiia <sychak.pn@ucu.edu.ua>
2025-04-06 20:22:38 +0300

    Inserts documentation and fixes Pylint.

--- Implement Queue using Stacks/solution.py
+++ Implement Queue using Stacks/solution.py
@@ -1,41 +1,70 @@
+"""Implement Queue using Stacks"""
 class Stack:
+    """
+    A basic implementation of a stack data structure.
+    """
     def __init__(self):
+        """
+        Initializes an empty stack.
+        """
         self.stack = []
 
     def push(self, x):
+        """
+        Adds an element to the top of the stack.
+        """
         self.stack.append(x)
-    
+
     def pop(self):
+        """
+        Removes and returns the element at the top of the stack.
+        """
         if self.is_empty():
             raise IndexError
         return self.stack.pop()
 
     def peek(self):
+        """
+        Returns the element at the top of the stack without \
+removing it.
+        """
         if self.is_empty():
             raise IndexError
         return self.stack[-1]
 
     def size(self):
+        """
+        Returns the number of elements in the stack.
+        """
         return len(self.stack)
 
     def is_empty(self):
+        """
+        Checks if the stack is empty.
+        """
         return not self.stack
-    
+
 class MyQueue(object):
+    """
+    An implementation of a queue data structure using two stacks.
+    This implementation follows the First-In-First-Out (FIFO) principle.
+    """
     def __init__(self):
+        """
+        Initializes an empty queue with two stacks.
+        """
         self.first_stack = Stack()
         self.second_stack = Stack()
 
     def push(self, x):
         """
-        :type x: int
-        :rtype: None
+        Adds an element to the back of the queue.
         """
         self.first_stack.push(x)
 
     def pop(self):
         """
-        :rtype: int
+        Removes and returns the element at the front of the queue.
         """
         if self.second_stack.is_empty():
             while not self.first_stack.is_empty():
@@ -44,25 +73,16 @@ class MyQueue(object):
 
     def peek(self):
         """
-        :rtype: int
+        Returns the element at the front of the queue without removing it.
         """
         if self.second_stack.is_empty():
             while not self.first_stack.is_empty():
                 self.second_stack.push(self.first_stack.pop())
         return self.second_stack.peek()
-        
 
     def empty(self):
         """
-        :rtype: bool
+        Checks if the queue is empty.
         """
         return self.first_stack.is_empty() and self.second_stack.is_empty()
-        
 
-
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
